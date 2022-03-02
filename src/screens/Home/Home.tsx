529b09e3f952dc2de7c0d99ff4e2c2cd11fc23a4
Jassel <ohjjmm@gmail.com>
2022-03-02 10:54:09 -0400

    added pagerview

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import {HomeScreenNavigationProp} from '../../types';
-import {SafeAreaView, View} from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import {ScaledSheet} from 'react-native-size-matters';
 import {Text, Button, TextInput} from '../../components';
 import {Theme} from '../../theme';
@@ -32,39 +39,46 @@ const Home = (props: Props) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text
-        label={'Before we start...'}
-        size={'largeTitle'}
-        textAlignment={'center'}
-        medium
-      />
-      <View style={styles.inputContainer}>
-        <TextInput
-          onChangeText={handleChange('name')}
-          onBlur={handleBlur('name')}
-          error={errors.name && touched.name ? true : false}
-          value={values.name}
-          label={'Name'}
-        />
-        {errors.name && <ValidationError error={errors.name} />}
-        <TextInput
-          onChangeText={handleChange('breed')}
-          onBlur={handleBlur('breed')}
-          error={errors.breed && touched.breed ? true : false}
-          value={values.breed}
-          label={'Favorite dog breed'}
-        />
-        {errors.breed && touched.breed && (
-          <ValidationError error={errors.breed} />
-        )}
-        <View style={styles.button}>
-          <Button
-            label={'Proceed'}
-            onPress={handleSubmit}
-            textProps={{color: 'white'}}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <Text
+            label={'Before we start...'}
+            size={'largeTitle'}
+            textAlignment={'center'}
+            medium
           />
-        </View>
-      </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              error={errors.name && touched.name ? true : false}
+              value={values.name}
+              label={'Name'}
+            />
+            {errors.name && touched.name && (
+              <ValidationError error={errors.name} />
+            )}
+            <TextInput
+              onChangeText={handleChange('breed')}
+              onBlur={handleBlur('breed')}
+              error={errors.breed && touched.breed ? true : false}
+              value={values.breed}
+              label={'Favorite dog breed'}
+            />
+            {errors.breed && touched.breed && (
+              <ValidationError error={errors.breed} />
+            )}
+            <View style={styles.button}>
+              <Button
+                label={'Proceed'}
+                onPress={handleSubmit}
+                textProps={{color: 'white'}}
+              />
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
--- src/screens/PageViewer/PageViewer.tsx
+++ src/screens/PageViewer/PageViewer.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {PageViewerScreenNavigationProp} from '../../types';
+import {View} from 'react-native';
+//import {PageViewerScreenNavigationProp} from '../../types';
+import PagerView from 'react-native-pager-view';
+import {ScaledSheet} from 'react-native-size-matters';
+import {Text} from '../../components';
+import {Theme} from '../../theme';
 
-type Props = {
+/* type Props = {
   navigation: PageViewerScreenNavigationProp;
-};
+}; */
 
-const PageViewer = (props: Props) => {
+const PageViewer = () => {
   return (
-    <View>
-      <Text>This is PageViewer</Text>
-    </View>
+    <PagerView style={styles.pagerView} initialPage={0} transitionStyle="curl">
+      <View key="1" style={styles.child}>
+        <Text label={'History1'} size={'largeTitle'} />
+      </View>
+      <View key="2" style={styles.child}>
+        <Text label={'History2'} size={'largeTitle'} />
+      </View>
+    </PagerView>
   );
 };
+
+const styles = ScaledSheet.create({
+  pagerView: {
+    flex: 1,
+  },
+  child: {
+    backgroundColor: Theme.colors.background,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default PageViewer;
